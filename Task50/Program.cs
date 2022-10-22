bc61d7665c49923ea65e1d67d74a8e7aaa39e97e
olgasuslova <alekssportmen@gmail.com>
2022-10-22 03:17:55 +0300

    50

---
+++ Task50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указывает, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+Console.Clear();
+
+Console.Write("Введите позицию элемента: ");
+int enternumber = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(5, 5, 0, 9);
+PrintArray(array);
+
+Console.WriteLine();
+
+if (array[i, j] == enternumber)
+                Console.WriteLine($"{enternumber}");
+            else 
+                Console.WriteLine($"{enternumber} - > такого числа в массиве нет.");
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
