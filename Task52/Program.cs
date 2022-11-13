75e5e5e40b49edc618d9deb0ca9d38dcf9d9c72c
olgasuslova <alekssportmen@gmail.com>
2022-11-13 16:01:33 +0300

    added 52

---
+++ Task52/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 9);
+Console.WriteLine();
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+ColumnAverage(array);
+
+
+/////////////////////////////////////////////////////////////
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+     {
+         for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ColumnAverage(int[,] inArray)
+{
+    double result;
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+           sum = sum + inArray[i, j]; 
+        }
+        result = Math.Round(sum / inArray.GetLength(1), 1);
+        Console.Write($"{result} ");
+    }
+}
