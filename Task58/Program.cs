f8acecba4a58276f238c97a3321e1abe80a2ddb6
OlgaSuslova <alekssportmen@gmail.com>
2022-11-15 16:21:43 +0300

    added 58

---
+++ Task58/Program.cs
@@ -0,0 +1,76 @@
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+
+Console.Clear();
+
+int[,] array1 = GetArray(2, 2, 0, 9);
+Console.WriteLine("Первая матрица:");
+PrintArray(array1);
+
+Console.WriteLine();
+
+int[,] array2 = GetArray(2, 2, 0, 9);
+Console.WriteLine("Вторая матрица:");
+PrintArray(array2);
+
+Console.WriteLine();
+
+int[,] resultarray = GetArrayProduct(array1, array2);
+Console.WriteLine("Произведение двух матриц:");
+PrintArray(resultarray);
+
+
+/////////////////////////////////
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+ int[,] GetArrayProduct(int[,] array1, int[,] array2)
+{
+    int[,] result = new int[array1.GetLength(0), array2.GetLength(1)];
+
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                result[i, j] += array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return result;
+}
+
+
+
+
+
+
